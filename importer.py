5186bdf22a4a7ee8ee43124a12adf6ba8a095719
aberguecio1 <48263883+aberguecio1@users.noreply.github.com>
2023-01-09 10:59:47 -0300

    beter code

--- importer.py
+++ importer.py
@@ -2,100 +2,104 @@ import os
 import request
 from bs4 import BeautifulSoup
 
-def formato_texto(texto):
-    nombre = texto.replace("%20"," ")
-    nombre = nombre.replace("&","and")
+def format_text(texto):
+    name = texto.replace("%20"," ")
+    name = name.replace("&","and")
     for l in ["a","e","i","o","u","A","E","I","O","U"]:
-        nombre=nombre.replace(f"{l}%CC%81",str(l))
-    return nombre
+        name=name.replace(f"{l}%CC%81",str(l))
+    return name
 
-def formato_nombre(texto):
-    nombre = texto.replace("&","and")
+def format_name(text):
+    name = text.replace("&","and")
     voc = ["a","e","i","o","u","A","E","I","O","U"]
     voc2 = ["á","é","í","ó","ú","Á","É","Í","Ó","Ú"]
     for x in range(10):
-        nombre=nombre.replace(voc2[x],voc[x])
-    return nombre
+        name=name.replace(voc2[x],voc[x])
+    return name
+
+def link_replace(links):
+    for link in links:
+        link_parts =str(link['href']).split("/")
+        if link_parts[0] != "https:":
+            name = format_text(str(link_parts[-1]))
+            if link_parts[-1][-5:] == ".html":
+                format_soup = format_soup.replace(str(link), "<ac:link><ri:page ri:content-title='"+name[:-5]+"' /><ac:plain-text-link-body> <![CDATA["+name[:-5]+"]]></ac:plain-text-link-body></ac:link>")
+            else:
+                format_soup = format_soup.replace(str(link), "<ac:link><ri:attachment ri:filename='"+name+"' /><ac:plain-text-link-body> <![CDATA["+name+"]]></ac:plain-text-link-body></ac:link>")
+    return format_soup
+
+def image_replace(images):
+    for image in images:
+        image_parts = str(image['src']).split("/")
+        if image_parts[0] != "https:":
+            format_soup = format_soup.replace(str(image), '<ac:image><ri:attachment ri:filename="'+str(image_parts[-1].replace("%20"," "))+'" /></ac:image>')
+    return format_soup
+
+def pae_update(response,pae,folder,file,space):
+        pae[0]+=1
+        if os.path.exists(folder+"/"+file[:-5]):
+            pae_hijo = content(folder+"/"+file[:-5], space, response["id"])
+            pae[0]+=pae_hijo[0]
+            pae[1]+=pae_hijo[1]
+            pae[2].update(pae_hijo[2])
+            pae[3].update(pae_hijo[3])
     
-def contenido(direccion, espacio, padre = False):
+def content(folder, space, father = False):
     pae = [0,0,{},{}]
-    lista = os.listdir(direccion)
-    for archibo in lista:
-        if archibo[-5:] == ".html":
-            nombre_pagina = formato_nombre(archibo[:-5])
+    list = os.listdir(folder)
+    for file in list:
+        if file[-5:] == ".html":
+            name_page = format_name(file[:-5])
 
             # Opening the html file
-            HTMLFile = open(direccion+"/"+archibo, "r", encoding = 'UTF8')
+            HTMLFile = open(folder+"/"+file, "r", encoding = 'UTF8')
             index = HTMLFile.read()
             soup = BeautifulSoup(index, 'lxml')
             format_soup = str(soup.body)
 
             # Remplaso imagenes
             images = soup.findAll('img')
-            for image in images:
-                image_parts = str(image['src']).split("/")
-                if image_parts[0] != "https:":
-                    format_soup = format_soup.replace(str(image), '<ac:image><ri:attachment ri:filename="'+str(image_parts[-1].replace("%20"," "))+'" /></ac:image>')
-            
+            format_soup = image_replace(images)
+
             # Remplaso links
             links = soup.findAll('a', href=True)
-            for link in links:
-                link_parts =str(link['href']).split("/")
-                if link_parts[0] != "https:":
-                    nombre = formato_texto(str(link_parts[-1]))
-                    if link_parts[-1][-5:] == ".html":
-                        format_soup = format_soup.replace(str(link), "<ac:link><ri:page ri:content-title='"+nombre[:-5]+"' /><ac:plain-text-link-body> <![CDATA["+nombre[:-5]+"]]></ac:plain-text-link-body></ac:link>")
-                    else:
-                        format_soup = format_soup.replace(str(link), "<ac:link><ri:attachment ri:filename='"+nombre+"' /><ac:plain-text-link-body> <![CDATA["+nombre+"]]></ac:plain-text-link-body></ac:link>")
-            
+            format_soup = link_replace(links)
+           
             #Send Post request whit body
-            print("Creando Pagina:",nombre_pagina)
-            respuesta = request.post_page(espacio,nombre_pagina,str(format_soup),padre)
-            if respuesta["statusCode"] == 200:
-                pae[0]+=1
-                if os.path.exists(direccion+"/"+archibo[:-5]):
-                    pae_hijo =contenido(direccion+"/"+archibo[:-5], espacio, respuesta["id"])
-                    pae[0]+=pae_hijo[0]
-                    pae[1]+=pae_hijo[1]
-                    pae[2].update(pae_hijo[2])
-                    pae[3].update(pae_hijo[3])
+            print("Creating Page:",name_page)
+            response = request.post_page(space,name_page,str(format_soup),father)
+            if response["statusCode"] == 200:
+                pae_update(response,pae,folder,file,space)
             else:
-                print("Error:\n",respuesta,"\nCreando Pagina sin formato:",nombre_pagina)
-                pae[2][nombre_pagina] = str(respuesta)
-                respuesta = request.post_page(espacio,nombre_pagina,str(soup.body),padre)
-                if respuesta["statusCode"] == 200:
-                    pae[0]+=1
-                    if os.path.exists(direccion+"/"+archibo[:-5]):
-                        pae_hijo =contenido(direccion+"/"+archibo[:-5], espacio, respuesta["id"])
-                        pae[0]+=pae_hijo[0]
-                        pae[1]+=pae_hijo[1]
-                        pae[2].update(pae_hijo[2])
-                        pae[3].update(pae_hijo[3])
-                else:
-                    print("\nError Critico:\n",respuesta,"\nSaltando subcarpetas...")
-                    pae[3][nombre_pagina] = str(respuesta)
+                print("Error:\n",response,"\nCreating Raw Page:",name_page)
+                pae[2][name_page] = str(response)
+                response = request.post_page(space,name_page,str(soup.body),father)
+                if response["statusCode"] == 200:
+                    pae_update(response,pae,folder,file,space)
+                    print("\nCritical Error:\n",response,"\nSkipping Subfolders...")
+                    pae[3][name_page] = str(response)
 
         # Attaching file
-        elif (archibo[-5] == "." or archibo[-4] == "."):
-            print(" -Adjuntando:",archibo)
-            respuesta = request.upload_file(direccion+"/"+archibo,padre)
-            if respuesta["statusCode"] == 200:
+        elif (file[-5] == "." or file[-4] == "."):
+            print(" -Attaching:",file)
+            response = request.upload_file(folder+"/"+file,father)
+            if response["statusCode"] == 200:
                 pae[1]+=1
             else:
-                print("\nError:\n",respuesta,"\n")
-                pae[2][archibo] = str(respuesta)
+                print("\nError:\n",response,"\n")
+                pae[2][file] = str(response)
     return pae
 
 
 if __name__ == "__main__":
-    fin = contenido('Export pro',"NI")
-    print("\n>>>   RESULTADOS   <<<")
-    print("Paginas creadas:",fin[0])
-    print("Archibos adjuntos:",fin[1])
-    print("Errores encontrados:",len(fin[2]))
+    fin = content('Export pro',"NI")
+    print("\n>>>   RESULTS   <<<")
+    print("Pages created:",fin[0])
+    print("Attached files:",fin[1])
+    print("Errors found:",len(fin[2]))
     for error in fin[2]:
         print(error,fin[2][error])
-    print("ERRORES CRITICOS!:",len(fin[3]))
+    print("CRITICAL ERRORS!:",len(fin[3]))
     for error in fin[3]:
         print(error,fin[3][error])
-    print("\n>>>   FIN   <<<\n")
+    print("\n>>>   END   <<<\n")
