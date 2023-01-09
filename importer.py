bdf459f0d8d347f41dd8327b2be762f89b54f2b4
aberguecio1 <48263883+aberguecio1@users.noreply.github.com>
2023-01-09 14:45:43 -0300

    clean names

--- importer.py
+++ importer.py
@@ -28,30 +28,53 @@ def link_replace(links,format_soup):
                 format_soup = format_soup.replace(str(link), "<ac:link><ri:page ri:content-title='"+name+"' /><ac:plain-text-link-body> <![CDATA["+name+"]]></ac:plain-text-link-body></ac:link>")
             else:
                 format_soup = format_soup.replace(str(link), "<ac:link><ri:attachment ri:filename='"+name+"' /><ac:plain-text-link-body> <![CDATA["+name+"]]></ac:plain-text-link-body></ac:link>")
-    return format_soup
 
 def image_replace(images,format_soup):
     for image in images:
         image_parts = str(image['src']).split("/")
         if image_parts[0] != "https:":
             format_soup = format_soup.replace(str(image), '<ac:image><ri:attachment ri:filename="'+str(image_parts[-1].replace("%20"," "))+'" /></ac:image>')
-    return format_soup
 
-def pae_update(response,pae,folder,file,space):
-    pae[0]+=1
-    if os.path.exists(folder+"/"+file[:-5]):
-        pae_hijo = content(folder+"/"+file[:-5], space, response["id"])
-        pae[0]+=pae_hijo[0]
-        pae[1]+=pae_hijo[1]
-        pae[2].update(pae_hijo[2])
-        pae[3].update(pae_hijo[3])
+def pae_update(pae,pae_hijo):
+    pae[0]+=pae_hijo[0]
+    pae[1]+=pae_hijo[1]
+    pae[2].update(pae_hijo[2])
+    pae[3].update(pae_hijo[3])
+
+def post_request(space,soup,format_soup,father,pae,folder,file,n=0):
+    name_page = format_name(file[:-5])
+    if n != 0:
+        name_page = name_page + str(n)
+    print("Creating Page:",name_page)
+    response = request.post_page(space,name_page,str(format_soup),father)
+    if response["statusCode"] == 200:
+        pae[0]+=1
+        if os.path.exists(folder+"/"+file[:-5]):
+            pae_hijo = content(folder+"/"+file[:-5], space, response["id"])
+            pae_update(pae,pae_hijo)
+    elif(response['message'][:106] == "com.atlassian.confluence.api.service.exceptions.BadRequestException: A page with this title already exists"):
+        print("A page with this title already exists:",name_page,)
+        n+=1
+        post_request(space,soup,format_soup,father,pae,folder,file,n)
+    else:
+        print("Error:\n",name_page,response,"\nCreating Raw Page:",name_page)
+        pae[2][name_page] = str(response)
+        response = request.post_page(space,name_page,str(soup.body),father)
+        
+        if response["statusCode"] == 200:
+            pae[0]+=1
+            if os.path.exists(folder+"/"+file[:-5]):
+                pae_hijo = content(folder+"/"+file[:-5], space, response["id"])
+                pae_update(pae,pae_hijo)
+        else:
+            print("\nCritical Error:\n",response,"\nSkipping Subfolders...")
+            pae[3][name_page] = str(response)
 
 def content(folder, space, father = False):
     pae = [0,0,{},{}]
     folder_content = os.listdir(folder)
     for file in folder_content:
         if file[-5:] == ".html":
-            name_page = format_name(file[:-5])
 
             # Opening the html file
             with open(folder+"/"+file, "r", encoding = 'UTF8') as html_file:
@@ -61,25 +84,14 @@ def content(folder, space, father = False):
 
             # Remplaso imagenes
             images = soup.findAll('img')
-            format_soup = image_replace(images,format_soup)
+            image_replace(images,format_soup)
 
             # Remplaso links
             links = soup.findAll('a', href=True)
-            format_soup = link_replace(links,format_soup)
+            link_replace(links,format_soup)
 
             #Send Post request whit body
-            print("Creating Page:",name_page)
-            response = request.post_page(space,name_page,str(format_soup),father)
-            if response["statusCode"] == 200:
-                pae_update(response,pae,folder,file,space)
-            else:
-                print("Error:\n",response,"\nCreating Raw Page:",name_page)
-                pae[2][name_page] = str(response)
-                response = request.post_page(space,name_page,str(soup.body),father)
-                if response["statusCode"] == 200:
-                    pae_update(response,pae,folder,file,space)
-                    print("\nCritical Error:\n",response,"\nSkipping Subfolders...")
-                    pae[3][name_page] = str(response)
+            post_request(space,soup,format_soup,father,pae,folder,file)
 
         # Attaching file
         elif (file[-5] == "." or file[-4] == "."):
@@ -94,7 +106,7 @@ def content(folder, space, father = False):
 
 
 if __name__ == "__main__":
-    end_data = content('Export pro',"CS")
+    end_data = content('Export pro',"PI")
     print("\n>>>   IFORMATION   <<<")
     print("Pages created:",end_data[0])
     print("Attached files:",end_data[1])
